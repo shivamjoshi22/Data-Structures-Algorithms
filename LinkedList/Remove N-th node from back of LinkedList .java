2063398c2a2fe26c94866fcafc041f85be7f8acd
Shivam Joshi <68525951+shivamjoshi22@users.noreply.github.com>
2020-07-23 13:57:16 +0530

    Remove N-th node from back of LinkedList

---
+++ LinkedList/Remove N-th node from back of LinkedList .java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode fast=head,slow=head;
+        if(n==1&&head.next==null)
+            return null;
+        while(n!=0)
+        {
+            fast=fast.next;
+            n--;
+        }
+        if(fast==null)
+        {
+            head=slow.next;
+        }
+    
+        else 
+        {
+            while(fast.next!=null)
+            {
+                slow=slow.next;
+                fast=fast.next;
+            }
+        }
+        
+        slow.next=slow.next.next;
+        return head;
+        
+        
+        
+    }
+}
